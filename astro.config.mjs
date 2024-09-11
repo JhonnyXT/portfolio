36b12631cbdd0eeca87ca12dd682c73cf8ffbc11
Jonathan Blandon <jonathanblandon1017@gmail.com>
2024-09-11 13:00:23 -0500

    Configura despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://jhonnyxt.github.io',
-  base: 'portfolio'
+  site: 'https://jhonnyxt.github.io/'
 })
